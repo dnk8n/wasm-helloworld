19bb269d1c193aecf0a6787efeb48ce3ca24483d
Dean Kayton <deankayton@gmail.com>
2023-02-02 13:39:03 +0200

    Try fix workflow

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Import our outputted wasm ES6 module
 // Which, export default's, an initialization function
-import init from "./hello_wasm.js";
+import init from "./pkg/hello_wasm.js";
 
 const runWasm = async () => {
   // Instantiate our wasm module
-  const helloWorld = await init("./hello_wasm_bg.wasm");
+  const helloWorld = await init("./pkg/hello_wasm_bg.wasm");
 
   // Call the Add function export from wasm, save the result
   const addResult = helloWorld.add(24, 24);
